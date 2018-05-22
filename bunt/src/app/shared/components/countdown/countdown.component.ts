67a9ed599a3bbe702dbc5abefcc0d55886c7de8e
Chyi, Lynn <lchyi@illumina.com>
2018-05-22 13:16:28 -0700

    fix timer issues

--- bunt/src/app/shared/components/countdown/countdown.component.ts
+++ bunt/src/app/shared/components/countdown/countdown.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, ApplicationRef } from '@angular/core';
 import * as moment from 'moment';
+import { Subject } from 'rxjs';
+import 'rxjs/add/operator/takeUntil';
 
 @Component({
   selector: 'bunt-countdown',
@@ -7,6 +9,8 @@ import * as moment from 'moment';
   styleUrls: ['./countdown.component.scss']
 })
 export class CountdownComponent implements OnInit, OnDestroy {
+  stable$: Subject<boolean> = new Subject<boolean>();
+
   @Input() untilDate: string;
   @Input() untilMsg: string;
   @Input() expiredMsg: string;
@@ -16,34 +20,46 @@ export class CountdownComponent implements OnInit, OnDestroy {
   minutes: number;
   seconds: number;
 
-  intervalId: number;
+  interval: any;
 
   expired = false;
 
-  constructor() {}
+  constructor(private applicationRef: ApplicationRef) {}
 
   ngOnInit() {
-    // const untilTime = new Date(this.untilDate).getTime();
+    setTimeout(() => {
+      this.interval = this.startTimer();
+    }, 200);
+    // this.applicationRef.isStable.takeUntil(this.stable$)
+    //   .subscribe(isStable => {
+    //     // Start timer when app becomes stable as a work around to register service workers
+    //     if (isStable) {
+    //       this.interval = this.startTimer();
+    //       this.stable$.next(true);
+    //     }
+    //   });
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
+  startTimer() {
     const untilTime = moment(this.untilDate).valueOf();
 
-    this.intervalId = setInterval(() => {
+    return setInterval(() => {
       const delta = untilTime - moment().valueOf();
 
       if (delta < 1000) {
         this.expired = true;
-        clearInterval(this.intervalId);
+        clearInterval(this.interval);
       }
-
+      // console.log('tick');
       // Time calculations for days, hours, minutes and seconds
       this.days = Math.floor(delta / (1000 * 60 * 60 * 24));
       this.hours = Math.floor((delta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       this.minutes = Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60));
       this.seconds = Math.floor((delta % (1000 * 60)) / 1000);
-    });
+    }, 1000);
   }
-
-  ngOnDestroy() {
-    clearInterval(this.intervalId);
-  }
-
 }
